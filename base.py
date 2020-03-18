387747aa8a588df44425ca08e0cceee647168794
RRG96 <rodrorx@gmail.com>
2020-03-18 01:26:19 -0600

    generalidad

--- base.py
+++ base.py
@@ -13,21 +13,54 @@ def parse():
     parser.add_argument('-su', '--ssh-user', action='store', required = True, type=str, help='Usuario de conexión a la máquina remota', metavar= 'SSH-USR', dest='su')
     return parser.parse_args()
 
-if __name__ == '__main__':
-    prs = parse()
-    print("La base de datos para drupal se creara con los siguientes parametros default " + prs.pd + " " + prs.pu + " " + prs.pp + " " + prs.pn)
-    print("Desea continuar?")
+def validacion():
+    print("Desea Continuar?\nSi (S), No (N)")
     do
     {
         opcion = readline()
-    }while(opcion == "S" or opcion == "N")
-    if opcion == "S":
-        print("Se descargaran dependencias necesarias para la migración")
+    }while(opcion == "S" or opcion == "N" or opcion == "s" or opcion == "n")
+    if opcion == "S" or opcion == "s":
+        True
+    else:
+        False
+
+def instalar_dependencias(prs.su, prs.pd, prs.pu, prs.pp, prs.pn):
+    print("Se descargaran dependencias necesarias para la migración")
+    if validacion():
         os.system("chmod 777 ../*")
         os.system("sudo dependencias.sh" + prs.su + " " + prs.pd + " " + prs.pu + " " + prs.pp + " " + prs.pn)
-        print("Se instalará drush y drupal" + prs.pd + " " + prs.pu + " " + prs.pp + " " + prs.pn)
+    else:
+        print("No se descargaron dependencias\nPueden ser necesarias para los procesos siguientes")
+
+def instalar_drupal(prs.pd, prs.pu, prs.pp, prs.pn):
+    print("Se instalará drush y drupal" + prs.pd + " " + prs.pu + " " + prs.pp + " " + prs.pn)
+    if validacion():
         os.system("sudo drush.sh")
-        print("Se comienza la migración")
-        os.system("sudo migracion.sh")
+    else:
+        print("No se instaló Drush ni Drupal\nPuede provocar problemas en el proceso futuro")
+     
+def configurar():
+    print("Se configurará Apache y PHP")
+    if validacion():
+        os.system("sudo configuracion.sh")
+    else:
+        print("No se configuraron los servicios\nBrechas de seguridad inminentes")
+        
+def migracion():
+    print("Se comienza la migración de drupal 7 a drupal 8")
+    if validacion:
+        os.system("sudo migracion.sh")   
+    else:
+        print("No se realizó la migración")
+
+if __name__ == '__main__':
+    prs = parse()
+    print("La base de datos para drupal se creará con los siguientes parámetros default " + prs.pd + " " + prs.pu + " " + prs.pp + " " + prs.pn)
+    if validacion():
+        instalar_dependencias()
+        configurar()
+        instalar_drupal()
+        migracion()
     else:
         print("Ingrese los valores al llamar al programa\nbase.py -pd algo -pu algo -pp algo -pn algo")
+        
