ffb0659eac716bf5dc4439b237cbb0bb2520633c
RRG96 <rodrorx@gmail.com>
2020-03-18 01:26:55 -0600

    .

--- dependencias.sh
+++ dependencias.sh
@@ -1,6 +1,7 @@
 #!bin/bash
 apt install -y php7.3
 apt install -y curl
+apt install apache2
 apt install -y postgresql-client-11
 curl -sS https://getcomposer.org/installer | php --install-dir=/usr/local/bin/composer
 ssh $1@$3 apt install -y postgresql-11 postgresql-client-11
