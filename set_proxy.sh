18c53f3f7a9eb0881e0ba151de0e2dbcd0421a55
magumbositos <magumbositos@waf-proxy.waf-proxy.local>
2020-03-20 14:59:15 -0600

    proxy

--- set_proxy.sh
+++ set_proxy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 set -e
-sudo cat proxy.conf > /etc/apache2/sites-available/proxy.conf
+sudo chmod 777 /etc/apache2/sites-available/
+sudo cat proxy.conf > /etc/apache2/sites-available/proxy.conf 
 sudo a2ensite proxy.conf
 sudo systemctl restart apache2
